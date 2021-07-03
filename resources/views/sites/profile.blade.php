f5334757ff01399fc573c1be3a5e4d8ddf691324
Ana <79823071+Ana2019-431@users.noreply.github.com>
2021-07-03 19:05:05 +0700

    Add files via upload

---
+++ resources/views/sites/profile.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.web')
+
+@section('content')
+
+<h4 >Indentitas</h4>
+		<img class="gambartengah" src="C:\Users\Wira\Pictures\265 Yudistira Dwi N..jpg">
+		<p>	Nama  : Mochammad Raffli Maulana </p>
+		<p> NIM   : 201910370311431  </p>	
+		<p>	TTL   : Pasuruan,9 September 2001 </p>
+		<p>	Hobby : Tidur </p>
+		<p>	Motto : Kerja tak mau, usaha tak mau. Tapi kalau membuat status "semua akan indah pada waktunya"/n
+                    Hey keong racun! Bangun! Bangun! Tidurmu terlalu miring </p>
+		<p>	Sekolah: Universitas Muhammadiyah Malang </p>
+
+    @stop    
